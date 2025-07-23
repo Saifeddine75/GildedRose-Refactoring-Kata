5d8fc038865df007f4b600581bcfbdf927ce0a3a
Saifeddine <saifeddine.bejaoui@gmail.com>
2025-07-23 13:25:59 +0200

    Refactor business logic with factory
    Create new Item subclasses:
    - NormalItem
    - Sulfuras
    - AgedBrie
    - BackstagePass

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,38 +3,37 @@
 class GildedRose(object):
 
     def __init__(self, items):
-        self.items = items
+        self.items = [ItemFactory.create(item) for item in items]
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            print("Item before update:", item.sell_in, item.quality)
+            item.update_quality()
+            item.sell_in -= 1
+            print("Item after update:", item.sell_in, item.quality)
 
+    @staticmethod
+    def increase_quality(item, amount=1):
+        if item.quality < 50:
+            item.quality += amount
+
+    @staticmethod
+    def decrease_quality(item, amount=1):
+        if item.quality > 0:
+            item.quality -= amount
+
+
+class ItemFactory:
+    @staticmethod
+    def create(item):
+        if item.name == "Aged Brie":
+            return AgedBrie(item.sell_in, item.quality)
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            return BackstagePass(item.sell_in, item.quality)
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            return Sulfuras(item.sell_in, item.quality)
+        else:
+            return NormalItem(item.name, item.sell_in, item.quality)
 
 class Item:
     def __init__(self, name, sell_in, quality):
@@ -44,3 +43,43 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+
+
+class NormalItem(Item):
+    def update_quality(self):
+        if self.sell_in < 0:
+            GildedRose.decrease_quality(self, 2)
+        else:
+            GildedRose.decrease_quality(self)
+
+
+class Sulfuras(Item):
+    def __init__(self, sell_in, quality):
+        super().__init__("Sulfuras, Hand of Ragnaros", sell_in, quality)
+
+    def update_quality(self):
+        pass  # Sulfuras does not need to update quality
+
+
+class AgedBrie(Item):
+    def __init__(self, sell_in, quality):
+        super().__init__("Aged Brie", sell_in, quality)
+
+    def update_quality(self):
+        GildedRose.increase_quality(self)
+
+
+class BackstagePass(Item):
+    def __init__(self, sell_in, quality):
+        super().__init__("Backstage passes to a TAFKAL80ETC concert", sell_in, quality)
+
+    def update_quality(self):
+        if self.sell_in < 1:
+            self.quality = 0
+        elif self.sell_in < 6:
+            GildedRose.increase_quality(self, 3)
+        elif self.sell_in < 11:
+            GildedRose.increase_quality(self, 2)
+        else:
+            GildedRose.increase_quality(self)
--- python/tests/test_gilded_rose.py
+++ python/tests/test_gilded_rose.py
@@ -5,12 +5,36 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_normal_item_degrades(self):
+        items = [Item("normalItem", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        update_item = gilded_rose.items[0]
+        self.assertEqual(9, update_item.sell_in)
+        self.assertEqual(19, update_item.quality)
 
-        
+    def test_aged_brie_increases(self):
+        items = [Item("Aged Brie", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        update_item = gilded_rose.items[0]
+        self.assertEqual(4, update_item.sell_in)
+        self.assertEqual(11, update_item.quality)
+
+    def test_backstage_pass_increases_by_3_when_5_days_left(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        update_item = gilded_rose.items[0]
+        self.assertEqual(4, update_item.sell_in)
+        self.assertEqual(13, update_item.quality)
+
+    def test_sulfuras_never_changes(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        update_item = gilded_rose.items[0]
+        self.assertEqual(80, update_item.quality)
+
 if __name__ == '__main__':
     unittest.main()
